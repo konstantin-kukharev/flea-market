ae85f2e167deced012ddf48cf534237ef35c5e2c
Konstantin Kukharev <i@kkukharev.ru>
2025-03-04 00:05:29 +0300

    fixed dev env

--- frontend/src/pages/IndexPage/IndexPage.tsx
+++ frontend/src/pages/IndexPage/IndexPage.tsx
@@ -6,15 +6,54 @@ import axios from 'axios';
 import { Link } from '@/components/Link/Link.tsx';
 import { Pagination } from '@/components/Pagination/Pagination.tsx';
 import { Page } from '@/components/Page.tsx';
+import ProductData from "./ProductData";
 
 import productSvg from './product.svg';
 
 const httpClient = axios.create({
-  baseURL: 'https://neighborline.ru/api',
+  baseURL: 'http://localhost/api',//import.meta.env.BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+const productsmock = [
+  {
+    _id: "100001",
+    img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    productName: "Round Table Clock",
+    price: "44.00",
+    color: "Black",
+  },
+  {
+    _id: "100002",
+    img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    productName: "Round Table Clock",
+    price: "44.00",
+    color: "Black",
+  },
+  {
+    _id: "100003",
+    img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    productName: "Round Table Clock",
+    price: "44.00",
+    color: "Black",
+  },
+  {
+    _id: "100004",
+    img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    productName: "Round Table Clock",
+    price: "44.00",
+    color: "Black",
+  },
+  {
+    _id: "100005",
+    img: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    productName: "Round Table Clock",
+    price: "44.00",
+    color: "Black",
+  },
+];
 
 const detailUrl = function( ID :number) { return `/product/${ID}`; };
 
@@ -68,7 +107,8 @@ export const IndexPage: FC = () => {
         <Section
           header="Товары"
           footer="Новопокровское"
-        >{products ? (
+        >
+          {/* {products ? (
             products.map(product => (
               <Link to={detailUrl(product.id)} key={product.id}><Cell
                 before={<Image src={productSvg} style={{ backgroundColor: '#464646' }}/>}
@@ -81,7 +121,21 @@ export const IndexPage: FC = () => {
             ))
           ) : (
             <Cell>Загрузка данных...</Cell>
-          )}
+          )} */}
+          <div className="container mx-auto my-96">
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-4">
+        {productsmock.map((product, index) => (
+          <ProductData
+            key={index}
+            _id={product._id}
+            img={product.img}
+            productName={product.productName}
+            price={product.price}
+            color={product.color}
+          />
+        ))}
+      </div>
+    </div>
           <Pagination paginator={paginator} />
           <Button onClick={fetchProducts}>Обновить данные</Button>
         </Section>
